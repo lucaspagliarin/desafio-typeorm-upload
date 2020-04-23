d1a93c2b9f61a12fcf8bc3b26cc56ddaf996b0b0
Lucas Pagliarin <lucas.pagliarin@gmail.com>
2020-04-23 15:54:57 -0300

    Changes made to returns and cors added

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -24,14 +24,17 @@ class Transaction {
   @Column()
   value: number;
 
+  @Column()
+  category_id: string;
+
   @ManyToOne(() => Category)
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
-  @CreateDateColumn({ select: false })
+  @CreateDateColumn()
   created_at: Date;
 
-  @UpdateDateColumn({ select: false })
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
